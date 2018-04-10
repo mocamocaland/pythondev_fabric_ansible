f9c071f9acd5fb01b26fc33ab32378395c692abc
mocamoca <noi_kaas@yahoo.co.jp>
2018-04-10 13:40:41 +0900

    env内容を追記

--- ops/fab_project/fabfile.py
+++ ops/fab_project/fabfile.py
@@ -1,5 +1,17 @@
-from fabric.api import run
+from fabric.api import run, env,roles
 
+env.hosts = ['root@172.16.200.101:22', 'root@172.16.200.102:22']
 
+env.passwords = {
+    'root@172.16.200.101:22': 'root',
+    'root@172.16.200.102:22': 'root',
+}
+
+env.roledefs = {
+    'web': ['root@172.16.200.101:22'],
+    'db': ['root@172.16.200.102:22']
+}
+
+@roles('web')
 def host_type():
     run('uname -s')
